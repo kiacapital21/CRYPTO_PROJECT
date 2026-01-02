a309bab7a71d8976601978d949139b503553cbce
EC2 Default User <ec2-user@ip-172-31-1-237.ap-south-1.compute.internal>
2026-01-02 20:13:17 +0530

    updating delay

--- src/services/delay.service.ts
+++ src/services/delay.service.ts
@@ -63,7 +63,7 @@ export class DelayService {
   }
 
   public async delay(): Promise<void> {
-    await this.waitUntilSameMinuteAtSecond(59, 850, false);
+    await this.waitUntilSameMinuteAtSecond(59, 800, false);
   }
 
   public async delayForStopLoss(): Promise<void> {
